fe2d039cd78c982b4e5bf176a890d061ebc2a6ea
falconraptor <falconraptor@users.noreply.github.com>
2018-12-05 01:04:17 -0800

    % Imports + Comments to most examples

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from server import route, serve, start_server, urls
+from server import route, serve, start_server, URLS, start_with_args
 
-__all__ = ['start_server', 'urls', 'route', 'serve']
+__all__ = ['start_server', 'URLS', 'route', 'serve', 'start_with_args']
--- example.py
+++ example.py
@@ -1,54 +1,54 @@
-from server import route, serve, start_server, urls
+from server import route, serve, start_server, URLS
 
 
-@route()
+@route()  # uses method name to generate url: /test/
 def test(request):
-    return 'Testing'
+    return 'Testing'  # return text only with default 200 status
 
 
-@route('example')
+@route('example')  # specify url directly: /example/
 def other(request):
-    return 'Other', None, {'Testing': 'Header'}
+    return 'Other', None, {'Testing': 'Header'}  # return text and header values with default 200 status
 
 
-@route('other/txt', methods=['post'])
+@route('other/txt', methods=['post'])  # specify url and lock to certain methods: anything but post will return 405
 def another(request):
-    return 'Txt', 204
+    return 'Txt', 204  # return text and status 204
 
 
-@route()
+@route()  # uses method name to generate url: /json/
 def json(request):
-    return request
+    return request  # return json version of request
 
 
-@route('(\d{2})')
+@route('(\d{2})')  # use regex groups to generate url: /[any 2 digit number]/
 def test2(request, num):
-    return 'Test2 [{}]'.format(num)
+    return 'Test2 [{}]'.format(num)  # return text only with default 200 status
 
 
-@route()
+@route()  # uses method name to generate url but because it is index: /
 def index(request):
-    return ['<a href="{}">{}</a><br>'.format(func.url, name) for name, func in urls.items()]
+    return ['<a href="{}">{}</a><br>'.format(func.url, name) for name, func in URLS.items()]  # return list of html which gets joined and sent to client
 
 
-@route()
+@route()  # uses method name to generate url: /index2/
 def index2(request):  # for use when len(urls) <= 3
-    return ['<a href="{}">{}</a><br>'.format(func.url, name) for name, func in urls.items()], 200
+    return ['<a href="{}">{}</a><br>'.format(func.url, name) for name, func in URLS.items()], 200  # return list of html which gets joined and sent to client with status 200
 
 
-@route('(?P<year>\d{4})/(?P<article>\d+)')
+@route('(?P<year>\d{4})/(?P<article>\d+)')  # use regex named groups to generate url: /[any 4 digit number]/[any number]/
 def article(request, article, year):
     return 'This is article {} from year {}'.format(article, year)
 
 
 @route()
 def readme(request):
-    return serve('README.md')
+    return serve('README.md')  # serve a file
 
 
 @route('([\w.]+)')
 def file(request, file):
-    return serve(file)
+    return serve(file)  # serve a parameter
 
 
 route('num/(?P<num>\d+)', f=test2)

b5b4784b9674c9e7269dbd98f0e62f52c5559c71
Dustin <dustinsurwill@gmail.com>
2020-06-15 00:04:47 -0700

    % github actions already having something on port 8000 and making tests fail

--- tests/test_example_with_spinup.py
+++ tests/test_example_with_spinup.py
@@ -1,4 +1,6 @@
+import _socket
 import mimetypes
+from contextlib import closing
 from random import randint
 from threading import Thread
 from typing import Iterable
@@ -16,19 +18,27 @@ def server():
 
 
 def _server():
-    s = example.start_server(serve=False)
+    port = find_free_port()
+    s = example.start_server(serve=False, port=port)
     t = Thread(target=s.serve_forever, daemon=True)
     t.start()
     while True:
         try:
-            requests.get('http://localhost:8000')
+            requests.get(f'http://localhost:{port}')
             break
         except ConnectionError:
             pass
-    return t
+    return port
 
 
-def url_test(url: str, allowed_methods: Iterable[str], expected_status: int, expected_result: bytes, expected_headers: dict = None, skip_405: bool = False, method_params: dict = None):
+def find_free_port():
+    with closing(_socket.socket(_socket.AF_INET, _socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(_socket.SOL_SOCKET, _socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
+
+def url_test(url: str, allowed_methods: Iterable[str], expected_status: int, expected_result: bytes, expected_headers: dict = None, skip_405: bool = False, method_params: dict = None, port: int = 8000):
     if url[-1:] != '/' and '.' not in url[-5:] and '?':
         url += '/'
     if not expected_headers:
@@ -36,13 +46,13 @@ def url_test(url: str, allowed_methods: Iterable[str], expected_status: int, exp
     if not method_params:
         method_params = {}
     if url[-1:] == '/':
-        result = requests.get(('http://localhost:8000/' + url[:-1]).replace(':8000//', ':8000/'), allow_redirects=False)
+        result = requests.get(f'http://localhost:{port}/{url[:-1]}'.replace(f':{port}//', f':{port}/'), allow_redirects=False)
         assert result.content == b''
         assert result.status_code == 307
         assert result.headers['Location'] == url or result.headers['Location'] == f'/{url}'
     allowed_methods = {method.upper() for method in allowed_methods}
     for method in ('GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'PUT'):
-        result = getattr(requests, method.lower())(('http://localhost:8000/' + url).replace('0//', '0/'), *([method_params] if method not in {'DELETE', 'OPTIONS'} else []))
+        result = getattr(requests, method.lower())(f'http://localhost:{port}/{url}'.replace(f':{port}//', f':{port}/'), *([method_params] if method not in {'DELETE', 'OPTIONS'} else []))
         if method in allowed_methods or '*' in allowed_methods:
             assert result.content == expected_result
             assert result.status_code == expected_status
@@ -54,62 +64,62 @@ def url_test(url: str, allowed_methods: Iterable[str], expected_status: int, exp
 
 
 def test_test(server):
-    url_test('/example/test/', ('*',), 200, b'Testing')
+    url_test('/example/test/', ('*',), 200, b'Testing', port=server)
 
 
 def test_other(server):
-    url_test('/example2/', ('*',), 200, b'Other', {'Testing': 'Header'})
+    url_test('/example2/', ('*',), 200, b'Other', {'Testing': 'Header'}, port=server)
 
 
 def test_another(server):
-    url_test('/other/txt/', ('POST',), 204, b'')
+    url_test('/other/txt/', ('POST',), 204, b'', port=server)
 
 
 def test_json(server):
     url = '/example/json/'
-    result = requests.get(('http://localhost:8000/' + url[:-1]).replace('0//', '0/'), allow_redirects=False)
+    result = requests.get(f'http://localhost:{server}/{url[:-1]}'.replace(f':{server}//', f':{server}/'), allow_redirects=False)
     assert result.content == b''
     assert result.status_code == 307
     assert result.headers['Location'] == url
     for method in ('GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'PUT'):
-        result = getattr(requests, method.lower())(('http://localhost:8000/' + url).replace('0//', '0/'))
+        result = getattr(requests, method.lower())(f'http://localhost:{server}/{url}'.replace(f':{server}//', f':{server}/'))
         result.json()
         assert result.status_code == 200
 
 
 def test_test2(server):
-    url_test('0', ('*',), 404, b'')
+    url_test('0', ('*',), 404, b'', port=server)
     i = randint(10, 100)
-    url_test(f'{i}', ('*',), 200, f'Test2 [{i}]'.encode())
-    url_test('123', ('*',), 404, b'')
-    url_test('/num/1234488/', ('*',), 200, b'Test2 [1234488]')
+    url_test(f'{i}', ('*',), 200, f'Test2 [{i}]'.encode(), port=server)
+    url_test('123', ('*',), 404, b'', port=server)
+    url_test('/num/1234488/', ('*',), 200, b'Test2 [1234488]', port=server)
 
 
 def test_index(server):
-    url_test('/example/', ('*',), 200, b''.join(f'<a href="{func.url}">{name}</a><br>'.encode() for name, func in App._urls.items()))
+    url_test('/example/', ('*',), 200, b''.join(f'<a href="{func.url}">{name}</a><br>'.encode() for name, func in App._urls.items()), port=server)
 
 
 def test_index2(server):
-    url_test('/example/index2/', ('*',), 200, b''.join(f'<a href="{func.url}">{name}</a><br>'.encode() for name, func in App._urls.items()))
+    url_test('/example/index2/', ('*',), 200, b''.join(f'<a href="{func.url}">{name}</a><br>'.encode() for name, func in App._urls.items()), port=server)
 
 
 def test_article(server):
-    url_test('10/12', ('*',), 404, b'')
+    url_test('10/12', ('*',), 404, b'', port=server)
     i = randint(1000, 10000)
-    url_test(f'{i}/1', ('*',), 200, f'This is article 1 from year {i}'.encode())
-    url_test('12341/123', ('*',), 404, b'')
+    url_test(f'{i}/1', ('*',), 200, f'This is article 1 from year {i}'.encode(), port=server)
+    url_test('12341/123', ('*',), 404, b'', port=server)
 
 
 def test_readme(server):
     with open('README.md', 'rb') as readme:
-        url_test('/example/readme/', ('*',), 200, readme.read(), {'Content-Type': mimetypes.guess_type('README.md')[0] or 'application/octet-stream'})
+        url_test('/example/readme/', ('*',), 200, readme.read(), {'Content-Type': mimetypes.guess_type('README.md')[0] or 'application/octet-stream'}, port=server)
 
 
 def test_file(server):
     with open('setup.py', 'rb') as file:
-        url_test('setup.py', ('*',), 200, file.read(), {'Content-Type': mimetypes.guess_type('setup.py')[0]})
+        url_test('setup.py', ('*',), 200, file.read(), {'Content-Type': mimetypes.guess_type('setup.py')[0]}, port=server)
 
 
 def test_render(server):
     with open('README.md', 'rt') as readme:
-        url_test('/example/render_example/', ('GET',), 200, readme.read().replace('~~test~~', 'pytest').encode(), method_params={'test': 'pytest'})
+        url_test('/example/render_example/', ('GET',), 200, readme.read().replace('~~test~~', 'pytest').encode(), method_params={'test': 'pytest'}, port=server)

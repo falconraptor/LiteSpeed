b0b3aaa898ec04f9e1dbfecbe986d6ac186e0541
Dustin Surwill <dustinsurwill@gmail.com>
2021-03-22 16:58:37 -0700

    % update packaging config to setup github actions to auto publish on release

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
+import os
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    version="1.1.4",
+    version=os.environ['VERSION'],
     packages=setuptools.find_packages(exclude=['__pycache__', 'tests']),
     package_data={'litespeed': ['html/*.html']},
     include_package_data=True
